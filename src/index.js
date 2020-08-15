7922d23ccdb3a8bc9a8cc3f75252022d39433c54
unknown <ar425@student.le.ac.uk>
2020-08-15 09:17:29 +0100

    adding files

--- src/index.js
+++ src/index.js
@@ -4,14 +4,48 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+class Post extends React.Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        count: 0
+      };
+    }
+
+
+    counter() {
+      this.setState({
+        count: this.state.count + 1
+      });
+    };
+    
+    
+
+    render(){
+      return(
+        <div className="post">
+          <p>This is the first post and it is wonderful.</p>
+          <button className="countr" onClick={() => this.counter()}>Like</button>
+          <p className="number">{this.state.count}</p>
+        </div>
+      );
+    }
+
+}
+
+
+
+ 
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Post />,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();

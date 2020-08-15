819d599a495fe6fbcce3c9ac5f744abfba9e674c
unknown <ar425@student.le.ac.uk>
2020-08-15 11:10:31 +0100

    Updates

---
+++ precache-manifest.d3963f48feb0119a8375f9ff7da15970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2799ce4a05d770935a8e36acdccce2",
+    "url": "/Drone_Defence_Counter/index.html"
+  },
+  {
+    "revision": "6805ec201f5d8261e6bd",
+    "url": "/Drone_Defence_Counter/static/css/main.59ceaa8f.chunk.css"
+  },
+  {
+    "revision": "959d3100c387519dc6bd",
+    "url": "/Drone_Defence_Counter/static/js/2.6589ab7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drone_Defence_Counter/static/js/2.6589ab7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6805ec201f5d8261e6bd",
+    "url": "/Drone_Defence_Counter/static/js/main.499a7101.chunk.js"
+  },
+  {
+    "revision": "b29d3ab0d32084f91d71",
+    "url": "/Drone_Defence_Counter/static/js/runtime-main.d2fdb242.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Drone_Defence_Counter/static/media/logo.5d5d9eef.svg"
+  }
+]);
